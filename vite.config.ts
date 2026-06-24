d7860f645003341445e848d0b2bed43ed2dc4721
Rifky Dwi Rahmat Prakoso <rifkyrahmat2006@gmail.com>
2026-06-24 10:00:57 +0700

    fix: vite config

--- vite.config.ts
+++ vite.config.ts
@@ -6,27 +6,27 @@ import laravel from 'laravel-vite-plugin';
 import { bunny } from 'laravel-vite-plugin/fonts';
 import { defineConfig } from 'vite';
 
-export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.tsx'],
-            refresh: true,
-            fonts: [
-                bunny('Instrument Sans', {
-                    weights: [400, 500, 600],
-                }),
-            ],
-        }),
-        inertia(),
-        react({
-            babel: {
-                plugins: ['babel-plugin-react-compiler'],
-            },
-        }),
-        tailwindcss(),
-        wayfinder({
-            formVariants: true,
-            disabled: process.env.NODE_ENV === 'production',
-        }),
-    ],
-});
+const plugins = [
+    laravel({
+        input: ['resources/css/app.css', 'resources/js/app.tsx'],
+        refresh: true,
+        fonts: [
+            bunny('Instrument Sans', {
+                weights: [400, 500, 600],
+            }),
+        ],
+    }),
+    inertia(),
+    react({
+        babel: {
+            plugins: ['babel-plugin-react-compiler'],
+        },
+    }),
+    tailwindcss(),
+];
+
+if (process.env.NODE_ENV !== 'production') {
+    plugins.push(wayfinder({ formVariants: true }));
+}
+
+export default defineConfig({ plugins });
